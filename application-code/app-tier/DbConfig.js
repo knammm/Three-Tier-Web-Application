562f268b60393f0b28b588edc1414ba59bb71c7f
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-10-31 00:21:16 +0700

    Update files

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,6 +1,6 @@
 module.exports = Object.freeze({
-    DB_HOST : '',
-    DB_USER : '',
-    DB_PWD : '',
-    DB_DATABASE : ''
+    DB_HOST : 'database-1.cluster-cxybgeeizyv4.ap-southeast-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'KhanhNam1234',
+    DB_DATABASE : 'webappdb'
 });
--- application-code/web-tier/src/components/Home/Home.js
+++ application-code/web-tier/src/components/Home/Home.js
@@ -5,10 +5,10 @@
     class Home extends Component {
         render () {
         return (
-            <div>
-            <p style={{ color: "white", position: "absolute", top: 0, left: 0 }}>Nguyen Khanh Nam</p>
-            <h1 style={{color:"white"}}>AWS 3-TIER WEB APP DEMO</h1>
-            <img src={architecture} alt="3T Web App Architecture" style={{height:400,width:825}} />
+          <div style={{ position: "relative" }}>
+            <h1 style={{ color: "white" }}>Nguyen Khanh Nam - 2153599</h1>
+            <h1 style={{ color: "white" }}>AWS 3-TIER WEB APP DEMO</h1>
+            <img src={architecture} alt="3T Web App Architecture" style={{ height: 400, width: 825 }} />
           </div>
         );
       }
