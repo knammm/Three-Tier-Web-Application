8cc1ddcf9d4b34b1991e61bf59bb8412c27b3ff1
Nam Nguyen <nam.nguyenolkmphy@hcmut.edu.vn>
2023-10-30 18:53:04 +0700

    Add a text

--- application-code/web-tier/src/components/Home/Home.js
+++ application-code/web-tier/src/components/Home/Home.js
@@ -6,6 +6,7 @@
         render () {
         return (
             <div>
+            <p style={{ color: "white", position: "absolute", top: 0, left: 0 }}>Nguyen Khanh Nam</p>
             <h1 style={{color:"white"}}>AWS 3-TIER WEB APP DEMO</h1>
             <img src={architecture} alt="3T Web App Architecture" style={{height:400,width:825}} />
           </div>
